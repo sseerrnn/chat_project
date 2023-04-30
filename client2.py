dc25c0f55bb7e8d07e4ee8cb9c836e5095a2fe21
sern <sern2001@hotmail.com>
2023-04-30 19:36:05 +0700

    Add update_online_list

--- client2.py
+++ client2.py
@@ -1,9 +1,9 @@
 # import all the required modules
 import socket
 import threading
+import time
 from tkinter import *
-from tkinter import font
-from tkinter import ttk
+from tkinter import font, ttk
 
 PORT = 21630
 HOST = "127.0.0.1"
@@ -11,9 +11,19 @@ HOST = "127.0.0.1"
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 
+# UTIL section
+def util_extract_data_list(message, start_idx = 2): # idx 0 is command and idx 1 is status
+    result = []
+    current_idx = start_idx
+    print("message util: ",message )
+    while current_idx < len(message):
+        name = message[current_idx+1 : current_idx + int(message[current_idx]) + 1]
+        current_idx = current_idx + int(message[current_idx]) + 1
+        result.append(" ".join(name))
+    print("result", result)
+    return result
+
 # GUI class for the chat
-
-
 class GUI:
     # constructor method
     def __init__(self):
@@ -73,6 +83,7 @@ class GUI:
     def goAhead(self, name):
         # get the root window object
         self.login.destroy()
+        self.name = name
         self.root = Tk()
         self.root.geometry("800x600")
         self.root.title("Information")
@@ -89,14 +100,17 @@ class GUI:
         self.users_listbox = Listbox(self.users_frame)
         self.users_listbox.pack(fill=BOTH, expand=True)
 
-        rcv1 = threading.Thread(target=self.rename(self.entryName.get()))
-        rcv1.start()
+        self.rename(name)
         
-        rcv2 = threading.Thread(target=self.get_online_list)
+        
+        rcv2 = threading.Thread(target=self.receive_msg)
         rcv2.start()
 
-        for i in range(5):
-            self.users_listbox.insert(END, f"User {i}")
+        # rcv3 = threading.Thread(target=self.get_group_list)
+        # rcv3.start()
+        
+        # for i in rcv2:
+        #     self.users_listbox.insert(END, f"User {i}")
 
         # create the "Available Groups" listbox and its header
         self.groups_frame = Frame(self.root)
@@ -121,22 +135,173 @@ class GUI:
         self.group_name_entry.pack(padx=5, pady=5, expand=True)
 
         self.selected_listbox = Button(
-            self.root, text='talk', command=lambda: print(self.get_selected_listbox()))
+            self.root, text='talk', command=self.talk_button_clicked)
         self.selected_listbox.pack(side=BOTTOM, padx=5, pady=5, expand=True)
 
         # for tag user
         # self.users_listbox.itemconfig(0, bg="yellow")
         self.root.mainloop()
+    
+    def talk_button_clicked(self):
+        selected_item , is_group = self.get_selected_listbox()
+        if is_group :
+            name = selected_item
+            self.member_list = []
+        else :
+            name = f"{selected_item} And {self.name}"  
+            member_list = [selected_item , self.name]
+        self.layout(name = name,member_list = member_list , is_group = is_group)
+              
+    def layout(self, name, member_list,is_group):
+        self.Window.protocol('WM_DELETE_WINDOW', lambda: self.Window.withdraw())
+        if is_group : member_list = self.get_group_member_list(name) 
+        self.name = name
 
+        # Show chat window
+        self.Window.deiconify()
+        self.Window.title(name)
+        self.Window.resizable(width=False, height=False)
+        self.Window.configure(width=470, height=550, bg="#17202A")
+
+        # Create a label for the group name
+        self.labelHead = Label(self.Window, bg="#17202A", fg="#EAECEE", text=self.name, font="Helvetica 13 bold", pady=5)
+        self.labelHead.place(relwidth=1)
+
+        # Create a text box for displaying the chat messages
+        self.textCons = Text(self.Window, width=20, height=2, bg="#17202A", fg="#EAECEE", font="Helvetica 14", padx=5, pady=5)
+        self.textCons.place(relheight=0.745, relwidth=1, rely=0.08)
+
+        # Create a label at the bottom for user input
+        self.labelBottom = Label(self.Window, bg="#ABB2B9", height=80)
+        self.labelBottom.place(relwidth=1, rely=0.825)
+
+        # Create an entry field for typing messages
+        self.entryMsg = Entry(self.labelBottom, bg="#2C3E50", fg="#EAECEE", font="Helvetica 13")
+
+        # Create a label to display the member list
+        members_label = Label(self.Window, bg="#17202A", fg="#EAECEE", text=f"Members count: {len(member_list)}", font="Helvetica 10 bold", pady=5)
+        members_label.place(relx=0.02, rely=0.05)
+
+        # Create a text box to display the member list
+        members_textbox = Text(self.Window, width=20, height=3, bg="#17202A", fg="#EAECEE", font="Helvetica 10", padx=5, pady=5)
+        members_textbox.place(relx=0.02, rely=0.1)
+
+        
+
+        # Insert member names into the text box
+        for member in member_list:
+            members_textbox.insert(END, member + '\n')
+
+        # Create a Send Button
+        self.buttonMsg = Button(self.labelBottom, text="Send", font="Helvetica 10 bold", width=20, bg="#ABB2B9", command=lambda: self.sendButton(self.entryMsg.get()))
+        self.buttonMsg.place(relx=0.77, rely=0.008, relheight=0.06, relwidth=0.22)
+        
+        # Create a button to leave the group
+        self.leave_group_button = Button(self.Window, text="Leave Group", font="Helvetica 10 bold", width=20, bg="#ABB2B9", command=lambda: self.leave_group())
+        
+        
+    def sendButton(self, msg):
+        self.textCons.config(state = DISABLED)
+        self.msg=msg
+        self.entryMsg.delete(0, END)
+        snd= threading.Thread(target = self.sendMessage)
+        snd.start()
+        
     def get_selected_listbox(self):
         """
         Returns the value of the item currently selected in the given listbox.
         """
         for i in self.users_listbox.curselection():
-            return self.users_listbox.get(i)
+            return (self.users_listbox.get(i),0)
+        
         for i in self.groups_listbox.curselection():
-            return self.groups_listbox.get(i)
+            return (self.groups_listbox.get(i),1) 
+    
+    
+    def receive_msg(self):
+        self.get_online_list()
+        while True:
+            message = client.recv(1024).decode("utf-8")
+            print("received message : ",message)
+            message = message[1:].split()
+            match message[0] :
+                case "broadcast":
+                    match message[1]:
+                        case "online":
+                            client.send("/online_list".encode("utf-8"))
+                        case "massage":
+                            continue
+                        case "group":
+                            self.update_group_list(message[1:])
+                        case _:
+                            print("no understandable message found")
+                case "create":
+                    # no response
+                    pass
+                case "dm":
+                    # no response
+                    pass
+                case "join" :
+                    if message[1] == "error":
+                        # TODO handle "join error Group not found" message
+                        pass
+                    else :
+                        messages_list = util_extract_data_list(message)
+                        # even_idx = message_context, odd_idx = message_sender
+                        for idx in range(0, len(messages_list), 2):
+                            message_context = messages_list[idx]
+                            message_sender = messages_list[idx+1]
+                            # TODO display it on chat box
+                            
+                case "leave" :
+                    if message[1] == "error":
+                        # TODO handle "leave error Group not found" message
+                        
+                        # TODO handle "leave error The user is not in this group" message
+                        pass
+                    else :
+                        # TODO handle "leave success"
+                        pass
+                case "message":
+                    if message[1] == "error":
+                        # TODO handle "message error Group not found" message
 
+                        # TODO handle "message error The user is not a member or owner of the group" message
+                        pass
+                    else :
+                        username_length = int(message[2])
+                        username = message[3:3+username_length]
+                        message_context = message[3+username_length:]
+                        # TODO display the message on chat box
+                case "rename":
+                    if message[1] == "error":
+                        # TODO handle "rename error The name is already taken" message
+                        pass
+                    else :
+                        old_username_length = int(message[2])
+                        old_username = message[3: 3+old_username_length]
+                        new_username_length = int(message[3+old_username_length])
+                        new_username = message[4+old_username_length:4+old_username_length+new_username_length]
+                        # TODO handle extracted data
+                case "online_list":
+                    username_list = util_extract_data_list(message, start_idx=2)
+                    self.update_online_list(username_list)
+                case "whoami":
+                    username_length = message[2]
+                    username = message[3:3+username_length]
+                    # TODO handel extracted data
+                case "group_member":
+                    if message[1] == "error":
+                        # TODO handle "group_member error Group not found" message
+                        pass
+                    else :
+                        member_list = util_extract_data_list(message, start_idx=2)
+                        print(member_list)
+                case "group_list":
+                    pass
+
+                    
+        
     def create_group(self):
         group_name = self.group_name_entry.get()
         # Add new group to the listbox
@@ -145,24 +310,66 @@ class GUI:
         self.group_name_entry.delete(0, END)
 
     def get_online_list(self):
-        while True:
-            try:
-                client.send("/online_list".encode("utf-8"))
-                online_list = client.recv(1024).decode("utf-8")
-                print("here")
-                print(online_list)
-                return online_list
-            except:
-                break
+        try:
+            client.send("/online_list".encode("utf-8"))
+        except:
+            pass
 
-    def rename(self,name):
-        while True :
-            try:     
-                client.send(f"/rename {len(name)} {name}".encode("utf-8"))
-                response = client.recv(1024).decode("utf-8")
-                print("rename to : "+response.split(" ")[3])
+    def update_online_list(self, username_list):
+        print("online_list: ",username_list)
+        self.users_listbox.delete(0,END)
+        for i in  username_list :
+            if i == self.name : self.users_listbox.insert(END, f"User :{i} (me)")
+            else :self.users_listbox.insert(END, f"User :{i}")
+        
+
+    def get_group_list(self):
+            try:
+                client.send("/group_list".encode("utf-8"))
             except:
-                break
-                
+                pass
+
+    def update_group_list(self, message):
+        print("group_list: ",message)
+
+        if message[0] == "success":
+            result = []
+            current_idx = 1
+            while current_idx < len(message):
+                name = message[current_idx+1 : current_idx + int(message[current_idx]) + 1]
+                current_idx = current_idx + int(message[current_idx]) + 1
+                result.append(" ".join(name))
+            for i in  result :
+                self.groups_listbox.insert(END, f"Group :{i}")
+        print("group result: ",result)       
+        return result 
+            
+    def get_group_member_list(self,group_name):
+        try:
+            client.send(f"/member_list {len(group_name)} {group_name}".encode("utf-8"))
+        except:
+            pass
+
+    def update_group_member_list(self, message):
+        pass
+            
+    def rename(self,name):
+        try:
+            if len(name.strip()) > 0:
+                client.send(f"/rename {len(name)} {name}".encode("utf-8"))
+                # response = client.recv(1024).decode("utf-8")
+                # print("rename to : "+response.split(" ")[3])
+                # print("rename response: ",response)
+                # break
+                # return response.split(" ")[3]
+        except:
+            pass
+
+    def sendMessage(self):
+        self.textCons.config(state=DISABLED)
+        while True:
+            message = f"{self.msg}"
+            client.send(message.encode("utf-8"))
+            break       
 # create a GUI class object
 g = GUI()
--- server.py
+++ server.py
@@ -1,8 +1,7 @@
-import socket
 import selectors
+import socket
 import types
 from datetime import datetime
-
 
 PORT = 21630
 HOST = "127.0.0.1"
@@ -32,7 +31,7 @@ def accept_wrapper(sock):
 	# broadcast to all user
 	for conn_t in connections:
 		if(conn == conn_t): continue
-		conn_t.send(b'/broadcast Online User Changed')
+		conn_t.send(b'/broadcast online')
 
 
 def service_connection(key, mask):
@@ -49,8 +48,7 @@ def service_connection(key, mask):
 				/exit
 				/messsage [len of group_name] [group_name] [message]
 				/whoami	
-				/rename 
-				[len of new_name] [new_name]
+				/rename [len of new_name] [new_name]
 				
 				/group_list
 				/group_member [len of group_name] [group_name]
@@ -58,12 +56,18 @@ def service_connection(key, mask):
 				
 
 			Send Protocol :
-				/error [res]
-				/success [res]		
+				/[function name] error [res]
+				/[function name] success [res]		
+
+			Broadcast :
+				/broadcast online => update online users list
+				/broadcast message [group_name_length] [group_name] [sender_name_lenght] [sender_name] [message_context] => update message in group
+				/broadcast group => update groups list
 			'''
 			if(recv_data[0] == '/'):
 				command = recv_data[1:].split(" ")
-				if(command[0] == 'create'):
+
+				if(command[0] == 'create' or command[0] == 'dm'):
 					group_name_length = int(command[1])
 					group_name = " ".join(command[2:2+group_name_length]).strip()
 					'''
@@ -72,14 +76,19 @@ def service_connection(key, mask):
 						owner : id
 						members : id[]
 						messages : [{context: string, date: date, sender: id}]
+						public : boolean
 					}
 					
 					'''
+					public = True
+					if(command[0] == 'dm') : public = False
+
 					group = {
 						"name": group_name,
 						"owner": users.get(sock).get("id"),
 						"members": [users.get(sock).get("id")],
-						"messages": []
+						"messages": [],
+						"public": public
 					}
 					groups.append(group)
 
@@ -89,7 +98,7 @@ def service_connection(key, mask):
 					group = list(filter(lambda group: group.get("name") == group_name, groups))
 					if(len(group) == 0):
 						# Handle group not found
-						recv_data = "/error Group not found"
+						recv_data = "/join error Group not found"
 					else :
 						group_index = groups.index(group[0])
 						if(group[0].get("owner") != users.get(sock).get("id") and  users.get(sock).get("id") not in group[0].get("members")) :
@@ -97,9 +106,11 @@ def service_connection(key, mask):
 						all_messages = ""
 						for message in groups[group_index].get("messages"):
 							context = message.get("context").strip()
+							username = id2name.get(message.get("sender").strip())
+							username_length = len(username.split(" "))
 							context_length = len(context.split(" "))
-							all_messages += f" {context_length} {context}"
-						recv_data = "/success" + " " + all_messages
+							all_messages += f" {context_length} {context} {username_length} {username}"
+						recv_data = "/join success" + " " + all_messages
 
 				elif(command[0] == 'leave'):
 					group_name_length = int(command[1])
@@ -107,17 +118,19 @@ def service_connection(key, mask):
 					group = list(filter(lambda group: group.get("name") == group_name, groups))
 					if(len(group) == 0):
 						# Handle group not found
-						recv_data = "/error Group not found"
+						recv_data = "/leave error Group not found"
 					elif(users.get(sock).get("id") not in group[0].get("members")):
-						recv_data = "/error The user is not in this group"
+						recv_data = "/leave error The user is not in this group"
 					else :
 						group_index = groups.index(group[0])
 						if groups[group_index].get("owner") == users.get(sock).get("id"):
 							groups.remove(group[0])
-							recv_data = "/success"
+							recv_data = "/leave success"
+							# TODO broadcast to close group chat window
 						else:
 							groups[group_index].get("members").remove(users.get(sock).get("id"))
-							recv_data = "/success"
+							recv_data = "/leave success"
+							# TODO broadcast to update group member
 						
 					
 
@@ -134,55 +147,52 @@ def service_connection(key, mask):
 				# 		del id2name[user_id]
 				# 	connections.remove(sock)
 					# for conn in connections:
-					# 	conn.send(b'/broadcast Online User Changed')
+					# 	conn.send(b'/broadcast online')
 				# 	del users[sock]
 				# 	sel.unregister(sock)
 				# 	sock.close()
 
 				elif(command[0] == 'message'):
 					user_id = users.get(sock).get("id")
-					username = users.get(sock).get("name")
+					username = users.get(sock).get("name").strip()
+					username_length = len(username.split(" "))
 					group_name_length = int(command[1])
 					group_name = " ".join(command[2:2+group_name_length]).strip()
 					message = " ".join(command[2+group_name_length:]).strip()
 					group = list(filter(lambda group: group.get("name") == group_name, groups))
 					if(len(group) == 0):
 						# Handle group not found
-						recv_data = "/error Group not found"
+						recv_data = "/message error Group not found"
 					elif(group[0].get("owner") != users.get(sock).get("id") and users.get(sock).get("id") not in group[0].get("members")):
 						# Handle owner join own group
-						recv_data = "/error The user is not a member or owner of the group"
+						recv_data = "/message error The user is not a member or owner of the group"
 					else :
 						group_index = groups.index(group[0])
 						groups[group_index].get("messages").append({"context": message, "date": datetime.now(), "sender": user_id})
-						recv_data = f"/success {username} " + message
+						recv_data = f"/message success {username_length} {username} " + message
 						group_conns = groups[group_index].get("members") + [groups[group_index].get("owner")]
 						# Change user_id to conn
 						id2conn = {value.get("id"):key for (key,value) in users.items()}
 						group_conns = [id2conn.get(id) for id in group_conns]
 						for conn in group_conns:
 							if(sock == conn): continue
-							conn.send((f"/broadcast {username} " + message).encode())
+							conn.send((f"/broadcast message {group_name_length} {group_name} {username_length} {username} " + message).encode())
 
 					
 				elif(command[0] == 'whoami'):
 					name = users.get(sock).get('name').strip()
 					name_length = len(name.split(" "))
-					recv_data = f"/success {name_length} {name}"
+					recv_data = f"/whoami success {name_length} {name}"
 
 				
 				elif(command[0] == 'group_list'):
-					group_string = " ".join([group.get("name").strip() for group in groups])
-					recv_data = "/success" + " " + group_string
 					all_group_name = ""
 					for group in groups:
+						if(group.get("public") == False) : continue
 						group_name = group.get("name").strip()
 						group_name_length = len(group_name.split(" "))
 						all_group_name += f" {group_name_length} {group_name}"
-					recv_data = "/success" + all_group_name
-
-				
-                
+					recv_data = "/group_list success" + all_group_name        
 
 				elif(command[0] == 'group_member'):
 					group_name_length = int(command[1])
@@ -190,40 +200,44 @@ def service_connection(key, mask):
 					group = list(filter(lambda group: group.get("name") == group_name, groups))
 					if(len(group) == 0):
 						# Handle group not found
-						recv_data = "/error Group not found"
+						recv_data = "/group_member error Group not found"
 					else :
 						group_index = groups.index(group[0])
 						all_messages = ""
 						for member in groups[group_index].get("members"):
 							mem_name = id2name.get(member).strip()
 							mem_name_length = len(mem_name.split(" "))
-							all_messages += f" {len(mem_name)} {mem_name}"
-						recv_data = "/success" + all_messages
+							all_messages += f" {mem_name_length} {mem_name}"
+						recv_data = "/group_member success" + all_messages
                         
                 
 				elif(command[0] == 'online_list'):
 					online = ""
 					for conn in connections:
-						if(sock == conn): continue
+						# if(sock == conn): continue
 						name = users.get(conn).get("name").strip()
 						name_length = len(name.split(" "))
 						online += f" {name_length} {name}"
-					recv_data = "/success" + online
+					recv_data = "/online_list success" + online
 
 				elif(command[0] == 'rename'):
 					user_id = users.get(sock).get("id")
 					username = users.get(sock).get("name").strip()
+					username_length = len(username.split(" "))
 					new_name_length = int(command[1])
 					new_name = " ".join(command[2:2+new_name_length]).strip()
 					if(new_name in id2name.values()):
-						recv_data = "/error The name is already taken"
+						recv_data = "/rename error The name is already taken"
 					else :
+						users[sock]["name"] = new_name
 						id2name[user_id] = new_name
-						recv_data = "/success " +len(username)+" "+username+ " "+len(new_name)+" "+username
-						recv_data = recv_data.strip()
+						print(users)
+						recv_data = "/rename success " +str(username_length)+" "+username+ " "+str(new_name_length)+" "+new_name
+						
 						for conn in connections:
 							if(sock == conn): continue
-							conn.send((f"/broadcast {username} " + new_name).encode())
+							# conn.send((f"/broadcast online {username_length} {username} {new_name_length} {new_name}").encode())
+							conn.send((f"/broadcast online").encode())
 
 			if recv_data:
 				data.outb += recv_data.encode()
@@ -241,20 +255,20 @@ def service_connection(key, mask):
 			if(user_id in id2name):
 				del id2name[user_id]
 			connections.remove(sock)
-			for conn in connections:
-				conn.send(b'/broadcast Online User Changed')
 			del users[sock]
 			sel.unregister(sock)
 			sock.close()
+			for conn in connections:
+				conn.send(b'/broadcast online')
 
 	if mask & selectors.EVENT_WRITE: # Should be ready to write
 		if data.outb:
 			print('Echoing', repr(data.outb), 'to', data.addr)
 			sent = sock.send(data.outb)  
 			# broadcast to all connections
-			for conn in connections:
-				if conn != sock:
-					conn.send(data.outb)
+			# for conn in connections:
+			# 	if conn != sock:
+			# 		conn.send(data.outb)
 			data.outb = data.outb[sent:] # remove from send buffer
 
 lsock.bind((HOST, PORT))
